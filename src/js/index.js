175c3e245e26534bc01378461d12af9dac42eea6
Anastasiya Sidarovich <anastasiya.sidarovich@digitech7.by>
2020-05-23 12:04:40 +0300

    fix: update reveal presentation

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,15 @@ Reveal.initialize({
   controls: true,
   touch: true,
   dependencies: [
-    {src: 'plugin/highlight/highlight.js', async: true },
+    { src: 'plugin/highlight/highlight.js', async: true },
     { src: 'plugin/notes/notes.js', async: true },
   ],
 });
+
+(function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = './images/favicon.ico';
+    document.getElementsByTagName('head')[0].appendChild(link);
+})();
